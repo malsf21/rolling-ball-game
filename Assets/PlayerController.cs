2b534683d99986ec0b0e0f4cc101a4f2a6912aa6
Matthew Wang <matt@matthewwang.me>
2018-10-16 00:10:30 -0700

    adds progress after day 1
    creates arena, player-controlled ball, camera that follows ball, and pickups

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 100;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        rb.AddForce(new Vector3(x, 0, z) * speed * Time.deltaTime);
+	}
+}
