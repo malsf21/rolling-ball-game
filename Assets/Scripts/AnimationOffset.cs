4962b56072bf034607442b2ce816f78ecf54e7c4
Matthew Wang <matt@matthewwang.me>
2018-10-29 21:59:51 -0700

    animations and scene switching!
    game is shaping up to something :)

---
+++ Assets/Scripts/AnimationOffset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationOffset : MonoBehaviour {
+
+    [Range(0, 1)]
+    public float offset;
+    public string animationName;
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        animator.Play(animationName, -1, offset);
+	}
+
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+    public void Load(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+}
